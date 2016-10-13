87b75425c73711cc5636ae81006ed3f247f6bb94
Jurre Wolsink <jurre.wolsink@gmail.com>
2016-10-13 22:32:41 +0200

    first assignment done

--- lab5-46.py
+++ lab5-46.py
@@ -1,86 +1,158 @@
-## Netwerken en Systeembeveiliging Lab 5 - Distributed Sensor Network
-## NAME:
-## STUDENT ID:
+# Netwerken en Systeembeveiliging Lab 5 - Distributed Sensor Network
+# NAME: Matthijs de Wit, Jurre Wolsink
+# STUDENT ID: 0000000, 10580476
 import sys
 import struct
 from socket import *
+from select import *
 from random import randint
 from gui import MainWindow
 from sensor import *
+from math import *
 
 
 # Get random position in NxN grid.
 def random_position(n):
-	x = randint(0, n)
-	y = randint(0, n)
-	return (x, y)
+    x = randint(0, n)
+    y = randint(0, n)
+    return (x, y)
 
 
-def main(mcast_addr,
-	sensor_pos, sensor_range, sensor_val,
-	grid_size, ping_period):
-	"""
-	mcast_addr: udp multicast (ip, port) tuple.
-	sensor_pos: (x,y) sensor position tuple.
-	sensor_range: range of the sensor ping (radius).
-	sensor_val: sensor value.
-	grid_size: length of the  of the grid (which is always square).
-	ping_period: time in seconds between multicast pings.
-	"""
-	# -- Create the multicast listener socket. --
-	mcast = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP)
-	# Sets the socket address as reusable so you can run multiple instances
-	# of the program on the same machine at the same time.
-	mcast.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
-	# Subscribe the socket to multicast messages from the given address.
-	mreq = struct.pack('4sl', inet_aton(mcast_addr[0]), INADDR_ANY)
-	mcast.setsockopt(IPPROTO_IP, IP_ADD_MEMBERSHIP, mreq)
-	if sys.platform == 'win32': # windows special case
-		mcast.bind( ('localhost', mcast_addr[1]) )
-	else: # should work for everything else
-		mcast.bind(mcast_addr)
+class Sensor:
+    def __init__(self, mcast_addr, sensor_pos, sensor_range, sensor_val,
+            grid_size, ping_period):
+        """
+        mcast_addr: udp multicast (ip, port) tuple.
+        sensor_pos: (x,y) sensor position tuple.
+        sensor_range: range of the sensor ping (radius).
+        sensor_val: sensor value.
+        grid_size: length of the  of the grid (which is always square).
+        ping_period: time in seconds between multicast pings.
+        """
+        self.mcast_addr = mcast_addr
+        self.sensor_pos = sensor_pos
+        self.sensor_range = sensor_range
+        self.sensor_val = sensor_val
+        self.neighbors = []
 
-	# -- Create the peer-to-peer socket. --
-	peer = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP)
-	# Set the socket multicast TTL so it can send multicast messages.
-	peer.setsockopt(IPPROTO_IP, IP_MULTICAST_TTL, 5)
-	# Bind the socket to a random port.
-	if sys.platform == 'win32': # windows special case
-		peer.bind( ('localhost', INADDR_ANY) )
-	else: # should work for everything else
-		peer.bind( ('', INADDR_ANY) )
+        # -- Create the multicast listener socket. --
+        mcast = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP)
+        # Sets the socket address as reusable so you can run multiple instances
+        # of the program on the same machine at the same time.
+        mcast.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
+        # Subscribe the socket to multicast messages from the given address.
+        mreq = struct.pack('4sl', inet_aton(mcast_addr[0]), INADDR_ANY)
+        mcast.setsockopt(IPPROTO_IP, IP_ADD_MEMBERSHIP, mreq)
+        if sys.platform == 'win32':  # windows special case
+            mcast.bind(('localhost', mcast_addr[1]))
+        else:  # should work for everything else
+            mcast.bind(mcast_addr)
 
-	# -- make the gui --
-	window = MainWindow()
-	window.writeln( 'my address is %s:%s' % peer.getsockname() )
-	window.writeln( 'my position is (%s, %s)' % sensor_pos )
-	window.writeln( 'my sensor value is %s' % sensor_val )
+        # -- Create the peer-to-peer socket. --
+        peer = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP)
+        # Set the socket multicast TTL so it can send multicast messages.
+        peer.setsockopt(IPPROTO_IP, IP_MULTICAST_TTL, 5)
+        # Bind the socket to a random port.
+        if sys.platform == 'win32':  # windows special case
+            peer.bind(('localhost', INADDR_ANY))
+        else:  # should work for everything else
+            peer.bind(('', INADDR_ANY))
 
-	# -- This is the event loop. --
-	while window.update():
-		pass
+        # -- make the gui --
+        window = MainWindow()
+        window.writeln('my address is %s:%s' % peer.getsockname())
+        window.writeln('my position is (%s, %s)' % sensor_pos)
+        window.writeln('my sensor value is %s' % sensor_val)
 
+        self.mcast = mcast
+        self.peer = peer
+        self.window = window
+
+    def send_ping(self):
+        self.neighbors = []
+        msg = message_encode(
+            MSG_PING,
+            0,
+            self.sensor_pos,
+            self.sensor_pos,
+            0,
+            self.sensor_range,
+            0
+        )
+        self.peer.sendto(msg, mcast_addr)
+        self.window.writeln('pinging...')
+
+    def recv_ping(self, initiator, init_range, addr):
+        dist = sqrt(pow(abs(initiator[0] - self.sensor_pos[0]), 2) +
+                pow(abs(initiator[1] - self.sensor_pos[1]), 2))
+
+        if round(dist) == 0 or dist > init_range:
+            return
+
+        self.window.writeln('ping recieved from: ' +
+                str(initiator[0]) + "," + str(initiator[1]))
+
+        self.send_pong(initiator, init_range, addr)
+
+    def send_pong(self, initiator, init_range, addr):
+        msg = message_encode(
+            MSG_PONG,
+            0,
+            initiator,
+            self.sensor_pos,
+            0,
+            init_range,
+            0
+        )
+        self.peer.sendto(msg, addr)
+        self.window.writeln('pong send')
+
+    def recv_pong(self, neighbor, addr):
+        self.window.writeln('pong recieved from: ' +
+                str(neighbor[0]) + "," + str(neighbor[1]))
+
+        self.neighbors.append([neighbor, addr])
+        print self.neighbors
+
+    def main(self):
+        # -- This is the event loop. --
+        while self.window.update():
+            rlist, _, _ = select([self.mcast, self.peer], [], [], 0)
+            for recv in rlist:
+                (data, addr) = recv.recvfrom(4096)
+                command, _, initiator, neighbor, _, init_range, _ = message_decode(data)
+                print command
+                if command == MSG_PING:
+                    self.recv_ping(initiator, init_range, addr)
+                elif command == MSG_PONG:
+                    self.recv_pong(neighbor, addr)
+
+            cmd = self.window.getline()
+            if cmd == 'ping':
+                self.send_ping()
 
 # -- program entry point --
 if __name__ == '__main__':
-	import sys, argparse
-	p = argparse.ArgumentParser()
-	p.add_argument('--group', help='multicast group', default='224.1.1.1')
-	p.add_argument('--port', help='multicast port', default=50000, type=int)
-	p.add_argument('--pos', help='x,y sensor position', default=None)
-	p.add_argument('--grid', help='size of grid', default=100, type=int)
-	p.add_argument('--range', help='sensor range', default=50, type=int)
-	p.add_argument('--value', help='sensor value', default=-1, type=int)
-	p.add_argument('--period', help='period between autopings (0=off)',
-		default=5, type=int)
-	args = p.parse_args(sys.argv[1:])
-	if args.pos:
-		pos = tuple( int(n) for n in args.pos.split(',')[:2] )
-	else:
-		pos = random_position(args.grid)
-	if args.value >= 0:
-		value = args.value
-	else:
-		value = randint(0, 100)
-	mcast_addr = (args.group, args.port)
-	main(mcast_addr, pos, args.range, value, args.grid, args.period)
+    import sys
+    import argparse
+    p = argparse.ArgumentParser()
+    p.add_argument('--group', help='multicast group', default='224.1.1.1')
+    p.add_argument('--port', help='multicast port', default=50000, type=int)
+    p.add_argument('--pos', help='x,y sensor position', default=None)
+    p.add_argument('--grid', help='size of grid', default=100, type=int)
+    p.add_argument('--range', help='sensor range', default=50, type=int)
+    p.add_argument('--value', help='sensor value', default=-1, type=int)
+    p.add_argument('--period', help='period between autopings (0=off)',
+                   default=5, type=int)
+    args = p.parse_args(sys.argv[1:])
+    if args.pos:
+        pos = tuple(int(n) for n in args.pos.split(',')[:2])
+    else:
+        pos = random_position(args.grid)
+    if args.value >= 0:
+        value = args.value
+    else:
+        value = randint(0, 100)
+    mcast_addr = (args.group, args.port)
+    sensor = Sensor(mcast_addr, pos, args.range, value, args.grid, args.period)
+    sensor.main()
